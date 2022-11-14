5d7735556c414b70ff78b56be10c5a8a835b8fc4
Roha-Lee <roha.lee@buzzvil.com>
2022-11-15 05:52:27 +0900

    practice using class variable

--- OOP/exercise_06.py
+++ OOP/exercise_06.py
@@ -1,10 +1,14 @@
 class User:
+    # 클래스 변수
+    count = 0
+    
     # 매직 메소드, 특수 메소드: 특정 상황에서 자동으로 호출되는 메소드 
     # init은 객체가 생성될 때 자동으로 호출된다. 
     def __init__(self, name, email, password):
         self.name = name
         self.email = email
         self.password = password
+        User.count += 1
     
     def __str__(self):
         return "이메일: {}, 이름: {}, 비밀번호: ******".format(self.email, self.name)
@@ -15,7 +19,5 @@ user2 = User("Yoonsoo", "yoonsoo@codeit.kr", "abcdef")
 user3 = User("Taeho", "taeho@codeit.kr", "123abc")
 user4 = User("Lisa", "lisa@codeit.kr", "abc123")
 
-print(user1)
-print(user2)
-print(user3)
-print(user4)
+# 모든 유저의 수를 나타내는 변수 
+print(User.count)
