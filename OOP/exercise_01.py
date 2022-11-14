fe8556d43d6176178901a467b0bef9355cbec364
Roha-Lee <roha.lee@buzzvil.com>
2022-11-15 04:53:57 +0900

    First OOP exercise

---
+++ OOP/exercise_01.py
@@ -0,0 +1,22 @@
+class User:
+    pass 
+
+user1 = User()
+user2 = User()
+user3 = User()
+
+# instance variable
+user1.name = "Roha"
+user1.email = "rohagru@gmail.com"
+user1.password = "12345"
+
+user2.name = "Roha2"
+user2.email = "rohagru2@gmail.com"
+user2.password = "12345"
+
+user3.name = "Roha3"
+user3.email = "rohagru3@gmail.com"
+user3.password = "12345"
+
+print(user1.email)
+print(user1.email)
