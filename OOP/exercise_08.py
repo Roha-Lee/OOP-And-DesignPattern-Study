ecbab5af6abde2bfd2fc609f1da064c38350c3a3
Roha-Lee <roha.lee@buzzvil.com>
2022-11-20 23:21:22 +0900

    using instance method instead of class method

--- OOP/exercise_08.py
+++ OOP/exercise_08.py
@@ -12,12 +12,14 @@ class User:
     def __str__(self):
         return "이메일: {}, 이름: {}, 비밀번호: ******".format(self.email, self.name)
     
-    # 클래스 메소드의 첫번째 인자는 cls를 적는 것을 규칙으로 한다. 
-    # cls는 유저 클래스를 나타낸다. 
-    @classmethod
-    def number_of_users(cls):
-        print("총 유저 수는 {}입니다.".format(cls.count))
-        
+    # # 클래스 메소드의 첫번째 인자는 cls를 적는 것을 규칙으로 한다. 
+    # # cls는 유저 클래스를 나타낸다. 
+    # @classmethod
+    # def number_of_users(cls):
+    #     print("총 유저 수는 {}입니다.".format(cls.count))
+    
+    def number_of_users(self):
+        print("총 유저 수는 {}입니다.".format(User.count))    
         
 
 user1 = User("Young", "young@codeit.kr", "123456")
@@ -25,7 +27,7 @@ user2 = User("Yoonsoo", "yoonsoo@codeit.kr", "abcdef")
 user3 = User("Taeho", "taeho@codeit.kr", "123abc")
 user4 = User("Lisa", "lisa@codeit.kr", "abc123")
 
-User.number_of_users()
+User.number_of_users(user1)
 user1.number_of_users()
 
 '''
